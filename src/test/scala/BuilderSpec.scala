606cbb49d3351e8c3fd2fee966464179d2eab39c
softprops <d.tangren@gmail.com>
2010-08-07 10:56:28 -0400

    example used provided types

--- src/test/scala/BuilderSpec.scala
+++ src/test/scala/BuilderSpec.scala
@@ -7,7 +7,7 @@ object BuilderSpec extends Specification {
   type MyAppType = { def apply(a:Map[String, Any]): (Int, Map[String, String], String) }
   
   "Builder" should {
-    "build and application with raw types" in {
+    "build an application with raw types" in {
       val app = new Function1[Map[String, Any], (Int, Map[String, String], String)] {
         def apply(e: Map[String, Any]) = (200, Map("Content-Type"->"text/plain"), "oh hai with env %s" format(e))
       }
@@ -29,6 +29,38 @@ object BuilderSpec extends Specification {
           }
         }
         def apply(a: MyAppType) = new MappAppending(a)
+      }
+
+      val (status, headers, body) = (new Builder use(() => mw1) use(() => mw2) run(app))(Map("foo"->"bar"))
+      
+      status must_==(200)
+      headers must havePair("Content-Type" -> "text/plain")
+      body must_==("mw1(mw2(oh hai with env Map(foo -> bar, baz -> boom)))")
+    }
+    
+    "builder an application with provided types" in {
+      import sing.Types._
+      val app = new Function1[Env, Response] {
+        def apply(e: Env) = (200, Map("Content-Type"->"text/plain"), "oh hai with env %s" format(e))
+      }
+
+      val mw1 = new Function1[App, App] {
+        def apply(a: MyAppType) = new Function1[Env, Response]{
+           def apply(e: Env) = {
+             val (status, headers, body) = a(e)
+             (status, headers, "mw1(%s)" format body)
+           }
+        }
+      }
+
+      object mw2 extends (App => App) {
+        class MappAppending(a: App) {
+          def apply(e: Env) = {
+             val (status, headers, body) = a(e + ("baz" -> "boom"))
+             (status, headers, "mw2(%s)" format body)
+          }
+        }
+        def apply(a: App) = new MappAppending(a)
       }
 
       val (status, headers, body) = (new Builder use(() => mw1) use(() => mw2) run(app))(Map("foo"->"bar"))
